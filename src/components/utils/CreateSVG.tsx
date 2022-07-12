6295fd9a3060fa3a5ecb2a9c8e77466118c8b686
slaidezera <slaidezera.web2021@gmail.com>
2022-07-12 02:23:25 -0300

    primeiro comite

---
+++ src/components/utils/CreateSVG.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="900"
+      height="200"
+      version="1.1"
+      viewBox="0 0 900 200"
+    >
+      <path fill="#1d1d1b" d="M0 0H900V200H0z"></path>
+      <path
+        fill="#1a1a18"
+        strokeLinecap="round"
+        d="M0 141l21.5-12.2C43 116.7 86 92.3 128.8 88c42.9-4.3 85.5 11.3 128.4 21.8 42.8 10.5 85.8 15.9 128.6 10 42.9-5.8 85.5-22.8 128.4-27.1 42.8-4.4 85.8 4 128.6 2.3 42.9-1.7 85.5-13.3 128.4-17 42.8-3.7 85.8.7 107.3 2.8L900 83V0H0z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/app'
+import '../styles/globals.scss'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
+}
